1701836e59ca6183c53202444447fde7aa3af949
1 <1@mail.ru>
2024-04-01 16:11:40 +0700

    start

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,18 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+
+
+conn_params = {
+  "host": "localhost",
+  "database": "north",
+  "user": "postgres",
+  "password": "catonnameG@v1"
+}
+
+
+with psycopg2.connect(**conn_params) as conn:
+    with conn.cursor() as cur:
+        with open('customers_data.csv', newline='') as file:
+            csv_file = csv.reader(file)
+            for row in csv_file:
+                cur.execute("INSERT INTO north VALUES (%s, %s, %s)", row)
