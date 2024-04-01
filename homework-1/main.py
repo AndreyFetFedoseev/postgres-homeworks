2dff24b7e2d500a9ca8519e787ef5236c3123133
Andrey Fedoseev <MaxviP100@yandex.ru>
2024-04-01 20:15:28 +0700

    add path customers_data

--- homework-1/main.py
+++ homework-1/main.py
@@ -10,22 +10,13 @@ conn_params = {
 }
 
 
-# with psycopg2.connect(**conn_params) as conn:
-#     with conn.cursor() as cur:
-#         with open('customers_data.csv', newline='') as file:
-#             csv_file = csv.reader(file)
-#             i = 0
-#             for row in csv_file:
-#                 if i != 0:
-#                     print(row)
-#                     cur.execute("INSERT INTO customers VALUES (%s, %s, %s)", row)
-#                 i += 1
-
-with psycopg2.connect(host='localhost', database='north', user='postgres', password='catonnameG@v1') as conn:
+with psycopg2.connect(**conn_params) as conn:
     with conn.cursor() as cur:
-        # cur.execute("INSERT INTO north VALUES (%s, %s)", (1, "Test"))
-        cur.execute("SELECT * FROM customers")
-
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
+        with open('homework-1/north_data/customers_data.csv', newline='') as file:
+            csv_file = csv.reader(file)
+            i = 0
+            for row in csv_file:
+                if i != 0:
+                    print(row)
+                    cur.execute("INSERT INTO customers VALUES (%s, %s, %s)", row)
+                i += 1
